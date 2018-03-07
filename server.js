9a6d06e855bd7048ffa58ae341b081723ea34d7f
ReginFell@gmail.com <SoulReaver1>
2018-03-07 23:07:30 +0200

    add webpack build add production instead dev

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-var express = require('express')
-var path = require('path')
-var serveStatic = require('serve-static')
+let express = require('express');
+let serveStatic = require('serve-static');
 
-app = express()
-app.use(serveStatic(__dirname))
-var port = process.env.PORT || 5000
-app.listen(port)
-console.log('server started ' + port)
+app = express();
+app.use(serveStatic(__dirname));
+let port = process.env.PORT || 5000;
+app.listen(port);
+
+console.log('server started ' + port);
