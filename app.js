e628fff9755526bebe2898c015f5b66773b0dad9
ReginFell <ReginFell@gmail.com>
2018-03-21 00:26:51 +0200

    Remove redundant server Add gitignore

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 let express = require('express');
+let serveStatic = require('serve-static');
 
 app = express();
+app.use(serveStatic(__dirname));
 let port = process.env.PORT || 5000;
 app.listen(port);
 
