79465f18dc9b2d89c58734d5971d3723c06487d1
ReginFell <ReginFell@gmail.com>
2018-03-21 15:06:54 +0200

    Add heroku config

---
+++ server.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const express = require('express');
+
+const app = express();
+
+const publicPath = express.static(path.join(__dirname, 'public'));
+
+app.use('/', publicPath);
+
+let port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log('Listening on port ' + port);
+});
