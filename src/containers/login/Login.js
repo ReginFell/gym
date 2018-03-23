c167d45d630f662964edeb5b5765d57c591cea92
ReginFell <ReginFell@gmail.com>
2018-03-23 16:43:34 +0200

    Add api Add signin

--- src/containers/login/Login.js
+++ src/containers/login/Login.js
@@ -26,7 +26,7 @@ class Login extends React.Component {
             <Container className="App">
                 <br/>
                 <CenterView>
-
+                    <Logo/>
                 </CenterView>
                 <CenterView>
                     <InputGroup>
