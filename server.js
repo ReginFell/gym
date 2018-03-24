1a816da841a74805a4c4ad188c199df010dd3fc2
Zabelnikov Sergey <ReginFell@gmail.com>
2018-03-24 08:37:48 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const app = express();
 
 const publicPath = express.static(path.join(__dirname, 'public'));
 
-app.use(publicPath);
+app.use('/*', 
+publicPath);
 
 let port = process.env.PORT || 5000;
 app.listen(port, () => {
