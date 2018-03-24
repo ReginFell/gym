d169fb272f83c5221307132e756c282199d4b298
Zabelnikov Sergey <ReginFell@gmail.com>
2018-03-24 08:54:43 +0200

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const publicPath = express.static(path.join(__dirname, 'public'));
 app.use('/*', 
 publicPath);
 
+app.get('*', function (_, res) { res.sendFile('public/index.html') });
+
 let port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log('Listening on port ' + port);
