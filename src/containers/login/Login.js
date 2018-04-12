ab6d0cb462211860702b217f2aec669c45ad843b
Serhii_Zabelnykov <serhii_zabelnykov@epam.com>
2018-04-12 16:36:58 +0300

    Change npm to yarn

--- src/containers/login/Login.js
+++ src/containers/login/Login.js
@@ -1,137 +1,148 @@
-import React from 'react';
-import {connect} from 'react-redux'
-import Logo from 'Resources/logo.nsvg';
-import './Login.css';
-import {login} from "Actions/login/LoginActions";
-import Button from 'material-ui/Button';
-import Paper from 'material-ui/Paper';
+import React from 'react'
+import { connect } from 'react-redux'
+import Logo from 'Resources/logo.nsvg'
+import { login } from 'Actions/login/LoginActions'
+import Button from 'material-ui/Button'
+import Paper from 'material-ui/Paper'
 import TextInputField from 'Components/global/TextInputField'
-import Typography from 'material-ui/Typography';
 import Progress from 'Components/global/Progress'
-import GoogleLogin from 'react-google-login';
-import {GOOGLE_API_KEY} from "Constants/config/services";
+import GoogleLogin from 'react-google-login'
+import { GOOGLE_API_KEY } from 'Constants/config/services'
 
-import {withStyles} from 'material-ui/styles';
+import { withStyles } from 'material-ui/styles'
 
 const styles = theme => ({
-    root: {
-        display: 'flex',
-        justifyContent: 'center',
-    },
-    paper: {
-        padding: theme.spacing.unit * 2,
-        display: 'flex',
-        width: 500,
-        alignItems: 'center',
-        flexDirection: 'column',
-    },
-    logo: {
-        height: 300
-    },
-    social_container: {
-        display: 'flex',
-        alignItems: 'center',
-    },
-    button: {
-        margin: theme.spacing.unit * 2,
-    },
-});
+  root: {
+    display: 'flex',
+    justifyContent: 'center'
+  },
+  paper: {
+    padding: theme.spacing.unit * 2,
+    display: 'flex',
+    width: 500,
+    alignItems: 'center',
+    flexDirection: 'column'
+  },
+  logo: {
+    height: 300
+  },
+  social_container: {
+    display: 'flex',
+    alignItems: 'center'
+  },
+  button: {
+    margin: theme.spacing.unit * 2
+  }
+})
 
 @withStyles(styles)
-@connect(state => ({
+@connect(
+  state => ({
     isLoading: state.app.isLoading,
     authError: state.login.authError,
     emailValidationError: state.login.emailValidationError,
     passwordValidationError: state.login.passwordValidationError
-}), {login: login})
-class Login extends React.Component {
+  }),
+  {login: login}
+)
+class Login extends React.Component{
+  constructor (props) {
+    super(props)
 
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            email: '',
-            password: ''
-        };
-
-        this.login = this.login.bind(this);
-        this.onPasswordChanged = this.onPasswordChanged.bind(this);
-        this.onEmailChanged = this.onEmailChanged.bind(this);
+    this.state = {
+      email: '',
+      password: ''
     }
 
-    login() {
-        this.props.login(this.state.email, this.state.password);
-    }
+    this.login = this.login.bind(this)
+    this.onPasswordChanged = this.onPasswordChanged.bind(this)
+    this.onEmailChanged = this.onEmailChanged.bind(this)
+  }
 
-    onEmailChanged(value) {
+  login () {
+    this.props.login(this.state.email, this.state.password)
+  }
 
-        this.setState(state => ({
-            email: value,
-            password: state.password
-        }))
-    }
+  onEmailChanged (value) {
+    this.setState(state => ({
+      email: value,
+      password: state.password
+    }))
+  }
 
-    onPasswordChanged(value) {
+  onPasswordChanged (value) {
+    this.setState((state) => ({
+      email: state.email,
+      password: value
+    }))
+  }
 
-        this.setState((state) => ({
-            email: state.email,
-            password: value
-        }))
-    }
+  render () {
+    const {classes} = this.props
 
-    render() {
-        const {classes} = this.props;
+    return (
+      <div className={classes.root}>
+        <Paper className={classes.paper}>
+          <Logo className={classes.logo}/>
+          <TextInputField
+            type='email-input'
+            id='email'
+            label='Email'
+            onChange={this.onEmailChanged}
+            value={this.state.email}
+            error={this.props.emailValidationError}
+          />
 
-        return (
-            <div className={classes.root}>
-                <Paper className={classes.paper}>
-                    <Logo className={classes.logo}/>
-                    <TextInputField
-                        type="email-input"
-                        id="email"
-                        label="Email"
-                        onChange={this.onEmailChanged}
-                        value={this.state.email}
-                        error={this.props.emailValidationError}/>
+          <TextInputField
+            id='password-input'
+            label='Password'
+            type='password'
+            autoComplete='current-password'
+            onChange={this.onPasswordChanged}
+            value={this.state.password}
+            error={this.props.passwordValidationError}
+          />
 
-                    <TextInputField
-                        id="password-input"
-                        label="Password"
-                        type="password"
-                        autoComplete="current-password"
-                        onChange={this.onPasswordChanged}
-                        value={this.state.password}
-                        error={this.props.passwordValidationError}/>
+          <div className={classes.social_container}>
+            <GoogleLogin
+              clientId={GOOGLE_API_KEY}
+              buttonText='Google Sign In'
+              onSuccess={response => {
+                console.log(response)
+              }}
+              onFailure={error => {
+                console.log(error)
+              }}
+            />
+          </div>
 
+          <Button
+            className={classes.button}
+            variant='raised'
+            color='primary'
+            onClick={this.login}
+          >
+            Авторизация
+          </Button>
 
-                    <div className={classes.social_container}>
-                        <GoogleLogin
-                            clientId={GOOGLE_API_KEY}
-                            buttonText="Google Sign In"
-                            onSuccess={(response) => {
-                                console.log(response)
-                            }}
-                            onFailure={(error) => {
-                                console.log(error)
-                            }}/>
-                    </div>
+          <Progress isLoading={this.props.isLoading}/>
 
-                    <Button className={classes.button} variant="raised" color="primary"
-                            onClick={this.login}>Авторизация</Button>
+          <Button
+            className={classes.button}
+            variant='raised'
+            color='secondary'
+            onClick={() => this.props.history.push('/signup')}
+          >
+            Регистрация
+          </Button>
 
-                    <Progress isLoading={this.props.isLoading}/>
-
-                    <Button className={classes.button}
-                            variant="raised" color="secondary"
-                            onClick={() => this.props.history.push('/signup')}>Регистрация</Button>
-
-                    {this.props.authError ? <div>Auth Error: {this.props.authError}</div> : null}
-                </Paper>
-            </div>
-        );
-    }
+          {this.props.authError ? (
+            <div>Auth Error: {this.props.authError}</div>
+          ) : null}
+        </Paper>
+      </div>
+    )
+  }
 }
 
-export default Login;
-
-
+export default Login
--- webpack.config.js
+++ webpack.config.js
@@ -2,62 +2,73 @@ const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
-    entry: [
-        'webpack-hot-middleware/client?quiet=true',
-        'react-hot-loader/patch',
-        './src/App.js'
-    ],
-    mode: 'development',
-    output: {
-        filename: 'bundle.js',
-        path: __dirname + "/public"
+  entry: [
+    'webpack-hot-middleware/client?quiet=true',
+    'react-hot-loader/patch',
+    './src/App.js',
+  ],
+  mode: 'development',
+  output: {
+    filename: 'bundle.js',
+    path: __dirname + '/public',
+  },
+  resolve: {
+    alias: {
+      Containers: path.resolve(__dirname, 'src/containers/'),
+      Resources: path.resolve(__dirname, 'src/resources/'),
+      Components: path.resolve(__dirname, 'src/components/'),
+      Reducers: path.resolve(__dirname, 'src/reducers/'),
+      Validators: path.resolve(__dirname, 'src/validators/'),
+      Constants: path.resolve(__dirname, 'src/constants/'),
+      Actions: path.resolve(__dirname, 'src/actions/'),
+      Api: path.resolve(__dirname, 'src/api/'),
+      Styles: path.resolve(__dirname, 'src/styles/'),
     },
-    resolve: {
-        alias: {
-            Containers: path.resolve(__dirname, 'src/containers/'),
-            Resources: path.resolve(__dirname, 'src/resources/'),
-            Components: path.resolve(__dirname, 'src/components/'),
-            Reducers: path.resolve(__dirname, 'src/reducers/'),
-            Validators: path.resolve(__dirname, 'src/validators/'),
-            Constants: path.resolve(__dirname, 'src/constants/'),
-            Actions: path.resolve(__dirname, 'src/actions/'),
-            Api: path.resolve(__dirname, 'src/api/'),
-            Styles: path.resolve(__dirname, 'src/styles/'),
+    extensions: ['.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        query: {
+          plugins: [
+            'babel-plugin-transform-decorators-legacy',
+            'babel-plugin-transform-decorators',
+          ],
+          presets: ['es2015', 'react'],
         },
-        extensions: ['.js', '.jsx']
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                loader: 'babel-loader',
-                query: {
-                    plugins: ['babel-plugin-transform-decorators-legacy', 'babel-plugin-transform-decorators'],
-                    presets: ['es2015', 'react']
-                },
 
-                exclude: /node_modules/
-            }, {
-                test: /\.scss$/,
-                loaders: ['style-loader', 'css-loader', 'sass-loader'],
-            }, {
-                test: /\.css$/,
-                loaders: ['style-loader', 'css-loader'],
-            }, {
-                test: /\.nsvg$/,
-                use: ['react-svg-loader']
-            }, {
-                test: /\.svg/,
-                use: {
-                    loader: 'svg-url-loader',
-                    options: {}
-                }
-            }
-        ]
-    }, plugins: [
-        new webpack.HotModuleReplacementPlugin()
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader', 'eslint-loader'],
+      },
+      {
+        test: /\.scss$/,
+        loaders: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.css$/,
+        loaders: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.nsvg$/,
+        use: ['react-svg-loader'],
+      },
+      {
+        test: /\.svg/,
+        use: {
+          loader: 'svg-url-loader',
+          options: {},
+        },
+      },
     ],
-    devServer: {
-        contentBase: "./public"
-    }
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()],
+  devServer: {
+    contentBase: './public',
+  },
 };
